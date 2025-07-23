3ee7da1e4853416482f6cd21449624771e30587f
Tyler Doering <thdoering3@gmail.com>
2025-07-22 21:16:19 -0700

    working on front end

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import NetWorthCard from './components/NetWorthCard';
 import CategoryBreakdown from './components/CategoryBreakdown';
 import TransactionList from './components/TransactionList';
+window.global = window;
+
 
 const App = () => (
   <div style={{ padding: '1rem', fontFamily: 'sans-serif' }}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/renderer/index.jsx',
@@ -8,30 +9,41 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  target: 'web', // use 'web' instead of 'electron-renderer' to fix global issues
   mode: 'development',
-  target: 'electron-renderer',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
   },
   resolve: {
     extensions: ['.js', '.jsx'],
     fallback: {
-      global: require.resolve('global')
+      global: require.resolve('global'),
+      buffer: require.resolve('buffer'),
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      url: require.resolve('url')
     }
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/renderer/index.html'
     }),
-    new NodePolyfillPlugin()
+    new NodePolyfillPlugin(),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+      global: require.resolve('global')
+    })
   ],
   devServer: {
     static: {
